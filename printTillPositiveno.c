f8b346dff59006c4fc6c97fd06aa61d5be1e27ec
DEBISHREENAYAK <57474704+DEBISHREENAYAK@users.noreply.github.com>
2021-09-02 22:21:58 +0530

    Create printTillPositiveno.c

---
+++ printTillPositiveno.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int
+main ()
+{
+  int n;
+
+  do
+    {
+      printf ("enter a number:");
+      scanf ("%d", &n);
+    }
+  while (n <= 0);
+  {
+    printf ("n value%d", n);
+  }
+  return 0;
+}
